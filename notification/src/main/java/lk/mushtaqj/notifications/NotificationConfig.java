afbae573af065d5a0025a03a73e50603ca45b6a9
Mushtaq Jameel <mushtaq.jameel@villvay.com>
2022-08-14 13:54:49 +0530

    adds queue config to notification config

--- notification/src/main/java/lk/mushtaqj/notifications/NotificationConfig.java
+++ notification/src/main/java/lk/mushtaqj/notifications/NotificationConfig.java
@@ -1,6 +1,11 @@
 package lk.mushtaqj.notifications;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -14,6 +19,25 @@ public class NotificationConfig
 
   @Value("${rabbitmq.routing-keys.internal-notification}")
   private String internalNotificationRoutingKey;
+
+  @Bean
+  public TopicExchange topicExchange(){
+    return new TopicExchange(this.exchangeName);
+  }
+
+  @Bean
+  public Queue notificationQueue(){
+    return new Queue(this.notificationQueueName);
+  }
+
+  @Bean
+  public Binding exchangeToQueueBinding(final TopicExchange topicExchange,
+                                        final Queue notificationQueue){
+
+    return BindingBuilder.bind(notificationQueue)
+                         .to(topicExchange)
+                         .with(this.internalNotificationRoutingKey);
+  }
 
   public String getExchangeName ()
   {
